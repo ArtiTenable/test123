305b3cd34e13651c2051eae1f5bc63fad69112c3
Accurics <admin@accurics.com>
2021-10-22 15:46:57 +0530

    Auto Remediation by Accurics, file: unknown

--- networking/networking/security-group.tf
+++ networking/networking/security-group.tf
@@ -13,7 +13,7 @@ resource "aws_security_group_rule" "vpc_endpoint_ingress" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = ["10.0.0.0/8"]
+  cidr_blocks = ["<cidr>"]
 
   security_group_id = aws_security_group.vpc_endpoint.id
 }
