c1e53e28424aa200e2aae9fd9d7dfcb26cf090c3
Accurics <admin@accurics.com>
2021-10-22 15:46:58 +0530

    Auto Remediation by Accurics, file: unknown

--- networking/networking/vpc.tf
+++ networking/networking/vpc.tf
@@ -51,3 +51,57 @@ resource "aws_default_vpc" "this" {
   )
 }
 
+
+resource "aws_flow_log" "this" {
+  vpc_id          = "${aws_vpc.this.id}"
+  iam_role_arn    = "<iam_role_arn>"
+  log_destination = "${aws_s3_bucket.this.arn}"
+  traffic_type    = "ALL"
+
+  tags = {
+    GeneratedBy      = "Accurics"
+    ParentResourceId = "aws_vpc.this"
+  }
+}
+resource "aws_s3_bucket" "this" {
+  bucket        = "this_flow_log_s3_bucket"
+  acl           = "private"
+  force_destroy = true
+
+  versioning {
+    enabled    = true
+    mfa_delete = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+resource "aws_s3_bucket_policy" "this" {
+  bucket = "${aws_s3_bucket.this.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "this-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            <principal_arn>
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.this.id}/*"
+    }
+  ]
+}
+POLICY
+}
